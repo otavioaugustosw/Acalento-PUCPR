35a2b4abbd039e7e132ae0d55798d57d4ba5e142
“Otávio <58156476+otavioaugustosw@users.noreply.github.com>
2025-04-21 23:56:01 -0300

    feat: validação basica de login

--- PROJETO/views/common/login.php
+++ PROJETO/views/common/login.php
@@ -56,3 +56,30 @@ if(isset($_POST['email'], $_POST['password'])) {
 </div>
 </body>
 </html>
+<?php
+if(isset($_POST['email'], $_POST['password'])) {
+    $result = $auth->authenticateUser($_POST['email'], $_POST['password']);
+    displayValidation('email', isValidEmail($_POST['email']));
+    displayValidation('password', hasContent($_POST['password']));
+    if ($result['status']) {
+        header('Location: index.php?common=1');
+        exit;
+    } else {
+       displayValidation('email', false);
+       displayValidation('password',false);
+        ?>
+        <div class="toast-container position-absolute p-3 top-0 start-50 translate-middle-x">
+            <div class="toast text-bg-danger border-0 show" id="toastRuim">
+                <div class="d-flex">
+                    <div class="toast-body">
+                        <?= $result['statusName'] == "BLOCK" ? "Tentativas excedidas, tente novamente mais tarde." : "E-mail ou senha incorretos."?>
+                    </div>
+                    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Fechar"></button>
+                </div>
+            </div>
+        </div>
+        <?php
+    }
+}
+
+?>

8fa3897860f92543c618b2cf33f7dad433b27720
“Otávio <58156476+otavioaugustosw@users.noreply.github.com>
2025-04-21 23:56:20 -0300

    feat: better structure

--- PROJETO/views/common/login.php
+++ PROJETO/views/common/login.php
@@ -2,30 +2,10 @@
 include(ROOT .'/php/config/session.php');
 include(ROOT . '/php/config/database_php.php');
 include(ROOT . '/php/auth_services/AuthService.php');
+include(ROOT . '/php/handlers/formValidator.php');
 
 $sql = connectDatabase();
 $auth = new AuthService($sql);
-
-if(isset($_POST['email'], $_POST['password'])) {
-    $result = $auth->authenticateUser($_POST['email'], $_POST['password']);
-    if ($result['status']) {
-        header('Location: index.php?common=1');
-        exit;
-    } else {?>
-        <div class="toast-container position-absolute p-3 top-0 start-50 translate-middle-x">
-            <div class="toast text-bg-danger border-0 show" id="toastRuim">
-                <div class="d-flex">
-                    <div class="toast-body">
-                      <?= $result['statusName'] == "BLOCK" ? "Tentativas excedidas, tente novamente mais tarde." : "E-mail ou senha incorretos."?>
-                    </div>
-                    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Fechar"></button>
-                </div>
-            </div>
-        </div>
-<?php
-    }
-}
-
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -34,10 +14,8 @@ if(isset($_POST['email'], $_POST['password'])) {
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
-    <link href="css/form-style.css" rel="stylesheet">
     <link href="css/default.css" rel="stylesheet">
-    <link href="css/login.css" rel="stylesheet">
+    <link href="css/form-style.css" rel="stylesheet">
 </head>
 <body class="d-flex justify-content-center align-items-center vh-100">
 <div class="card shadow-lg p-4">

46a069b2b524a699506ff880bd9a57872bd9c49c
“Otávio <58156476+otavioaugustosw@users.noreply.github.com>
2025-04-21 23:58:15 -0300

    feat: nova arrumação dos botões

--- PROJETO/components/sidebars/sidebars.php
+++ PROJETO/components/sidebars/sidebars.php
@@ -24,9 +24,13 @@
     <symbol id="puzzle" viewBox="0 0 16 16">
         <path d="M3.112 3.645A1.5 1.5 0 0 1 4.605 2H7a.5.5 0 0 1 .5.5v.382c0 .696-.497 1.182-.872 1.469a.5.5 0 0 0-.115.118l-.012.025L6.5 4.5v.003l.003.01q.005.015.036.053a.9.9 0 0 0 .27.194C7.09 4.9 7.51 5 8 5c.492 0 .912-.1 1.19-.24a.9.9 0 0 0 .271-.194.2.2 0 0 0 .036-.054l.003-.01v-.008l-.012-.025a.5.5 0 0 0-.115-.118c-.375-.287-.872-.773-.872-1.469V2.5A.5.5 0 0 1 9 2h2.395a1.5 1.5 0 0 1 1.493 1.645L12.645 6.5h.237c.195 0 .42-.147.675-.48.21-.274.528-.52.943-.52.568 0 .947.447 1.154.862C15.877 6.807 16 7.387 16 8s-.123 1.193-.346 1.638c-.207.415-.586.862-1.154.862-.415 0-.733-.246-.943-.52-.255-.333-.48-.48-.675-.48h-.237l.243 2.855A1.5 1.5 0 0 1 11.395 14H9a.5.5 0 0 1-.5-.5v-.382c0-.696.497-1.182.872-1.469a.5.5 0 0 0 .115-.118l.012-.025.001-.006v-.003l-.003-.01a.2.2 0 0 0-.036-.053.9.9 0 0 0-.27-.194C8.91 11.1 8.49 11 8 11s-.912.1-1.19.24a.9.9 0 0 0-.271.194.2.2 0 0 0-.036.054l-.003.01v.002l.001.006.012.025c.016.027.05.068.115.118.375.287.872.773.872 1.469v.382a.5.5 0 0 1-.5.5H4.605a1.5 1.5 0 0 1-1.493-1.645L3.356 9.5h-.238c-.195 0-.42.147-.675.48-.21.274-.528.52-.943.52-.568 0-.947-.447-1.154-.862C.123 9.193 0 8.613 0 8s.123-1.193.346-1.638C.553 5.947.932 5.5 1.5 5.5c.415 0 .733.246.943.52.255.333.48.48.675.48h.238z"/>
     </symbol>
-    <symbol id="pessoa" viewBox="0 0 16 16">
+    <symbol id="person" viewBox="0 0 16 16">
         <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
 
+    </symbol>
+
+    <symbol id="doorOpen" viewBox="0 0 16 16">
+        <path d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15zM11 2h.5a.5.5 0 0 1 .5.5V15h-1zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1"/>
     </symbol>
 </svg>
 
@@ -42,23 +46,21 @@
         <!-- sidebar navigation -->
         <ul class="sidebar-nav p-0 list-unstyled">
             <li class="sidebar-item mb-1">
-                <a  href="index.html" class="btn btn-toggle  d-inline-flex align-items-center rounded border-0 collapsed"
-                        data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
+                <a  href="index.php?common=1" class="btn btn-toggle  d-inline-flex align-items-center rounded border-0 collapsed">
                     <svg class="bi me-2" width="20" height="20" aria-hidden="true">
-                        <use xlink:href="#pessoa"/>
+                        <use xlink:href="#home"/>
                     </svg>
                     Página Inicial
                 </a>
             </li>
 
-            <li class="mb-1">
-                <button class="btn btn-toggle  d-inline-flex align-items-center rounded border-0 collapsed"
-                        data-bs-toggle="collapse" data-bs-target="#pessoa-collapse" aria-expanded="false">
+            <li class="sidebar-item mb-1">
+                <a  href="index.php?common=1" class="btn btn-toggle  d-inline-flex align-items-center rounded border-0 collapsed">
                     <svg class="bi me-2" width="20" height="20" aria-hidden="true">
-                        <use xlink:href="#home"/>
+                        <use xlink:href="#person"/>
                     </svg>
-                    Minha Conta
-                </button>
+                   Minha conta
+                </a>
             </li>
 
             <li class="sidebar-item mb-1">
@@ -92,7 +94,7 @@
                 <div class="collapse" id="doacoes-collapse">
                     <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
                                         <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Faça a sua primeira doação</a></li>
-                        <li><a href="../../php/pages/createDonation.php" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Cadastrar campanha</a></li>
+                        <li><a href="index.php?adm=1" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Cadastrar campanha</a></li>
                         <li><a href="../../php/pages/viewCampaign.php" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Editar campanha</a></li>
                         <li><a href="../../php/pages/createItem.php" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Cadastrar item</a></li>
                         <li><a href="../../php/pages/viewAllDonation.php" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Visualizar doações</a></li>
@@ -100,6 +102,15 @@
                                         <li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Suas doações</a></li>
                     </ul>
                 </div>
+            </li>
+
+            <li class="sidebar-item mb-1">
+                <a  href="index.php?common=3" class="btn btn-toggle  d-inline-flex align-items-center rounded border-0 collapsed">
+                    <svg class="bi me-2" width="20" height="20" aria-hidden="true">
+                        <use xlink:href="#doorOpen"/>
+                    </svg>
+                    Log Off
+                </a>
             </li>
         </ul>
         <!-- fim da navegão -->

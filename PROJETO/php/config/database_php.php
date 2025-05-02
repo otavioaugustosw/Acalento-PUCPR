0d53fd6e60c2cfae4b3cd212932ba6d5808096d7
“Otávio <58156476+otavioaugustosw@users.noreply.github.com>
2025-05-02 19:39:06 -0300

    fix: desfazer alterações

--- PROJETO/php/config/database_php.php
+++ PROJETO/php/config/database_php.php
@@ -1,22 +1,11 @@
 <?php
 function connectDatabase(): mysqli
 {
-    $isProduction = getenv('APP_ENV') === 'production';
-    if (!$isProduction) {
-        return new mysqli(
-            $_ENV['DATABASE_SERVER'],
-            $_ENV['DATABASE_USER'],
-            $_ENV['DATABASE_PASSWORD'],
-            $_ENV['DATABASE_NAME'],
-            $_ENV['DATABASE_PORT']
-        );
-    }
     return new mysqli(
-        getenv('DATABASE_SERVER'),
-        getenv('DATABASE_USER'),
-        getenv('DATABASE_PASSWORD'),
-        getenv('DATABASE_NAME'),
-        getenv('DATABASE_PORT')
+        $_ENV['DATABASE_SERVER'],
+        $_ENV['DATABASE_USER'],
+        $_ENV['DATABASE_PASSWORD'],
+        $_ENV['DATABASE_NAME'],
+        $_ENV['DATABASE_PORT']
     );
 }
-

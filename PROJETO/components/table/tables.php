a1f90b1bdc16e86baa31af022620c86f37e6c849
“Otávio <58156476+otavioaugustosw@users.noreply.github.com>
2025-05-04 03:57:34 -0300

    feat: modularizar table

---
+++ PROJETO/components/table/tables.php
@@ -0,0 +1,54 @@
+<?php
+function make_table_rows($table_rows)
+{
+?>
+
+<thead>
+<tr>
+    <?php
+    foreach ($table_rows as $rows){?>
+        <td><?= $rows ?></td>
+    <?php } ?>
+</tr>
+</thead>
+
+<?php
+}
+
+function make_table_head($table_columns)
+{
+?>
+
+<thead>
+<tr>
+<?php
+    foreach ($table_columns as $columns){?>
+        <th scope="col"> <?= $columns ?> </th>
+    <?php } ?>
+</tr>
+</thead>
+
+<?php
+}
+
+function render_donator_donations_table(array $table_columns, $donations)
+{
+    ?>
+    <table class="table table-hover table-amarela">
+        <?php
+        make_table_head($table_columns);
+        while ($donation = $donations->fetch_object()) {
+            $table_rows = [
+                $donation->opcao_nome,
+                $donation->quantidade,
+                $donation->tipo,
+                $donation->usuario_nome ?? 'Doador não cadastrado',
+                format_date($donation->data),
+                $donation->campanha_doacao_nome ?? 'Estoque'
+            ];
+            make_table_rows($table_rows);
+        }
+        ?>
+    </table>
+    <?php
+}

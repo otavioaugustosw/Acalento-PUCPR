b79214aef7b91dd33e9067c15ec33e632c124990
Anna Quézia <142363490+annaquezia@users.noreply.github.com>
2025-05-04 20:15:54 -0300

    feat: mudança na lógica

--- PROJETO/components/filter/filter.php
+++ PROJETO/components/filter/filter.php
@@ -1,9 +1,29 @@
 <?php
-function makeFilter()
+function makeFilter($donation = false)
 {?>
     <form method="POST" action="" class="mb-4">
         <div class="row g-3 align-items-center">
+        <?php if($donation) { ?>
+            <div class="col-md-3">
+                <select name="filtro" id="filtro" class="form-select" onchange="mostrarFiltroData()">
+                    <option value="">Selecione</option>
+                    <option value="passados">Passados</option>
+                    <option value="todos">Todos</option>
+                    <option value="mes">Último mês</option>
+                    <option value="data">Por dia</option>
+                </select>
+            </div>
 
+            <!-- Campo de data (visível condicionalmente) -->
+            <div class="col-md-3" id="filtroData" style="display: none;">
+                <input type="date" name="dia" id="dia" class="form-control">
+            </div>
+
+            <div class="col-md-2">
+                <?php makeButton("Filtrar", "btn btn-primary w-100", "", true );?>
+            </div>
+
+        <?php } else { ?>
             <!-- Campo de filtro -->
             <div class="col-md-3">
                 <select name="filtro" id="filtro" class="form-select" onchange="mostrarFiltroData()">
@@ -23,11 +43,9 @@ function makeFilter()
 
             <!-- Botão alinhado verticalmente com campos -->
             <div class="col-md-2 ">
-                <button type="submit" class="btn btn-primary w-100">
-                    Filtrar
-                </button>
+                <?php makeButton("Filtrar", "btn btn-primary w-100", "", true );?>
             </div>
-
+            <?php } ?>
         </div>
     </form>
 
